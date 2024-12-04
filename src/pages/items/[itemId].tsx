ce378261c8c055b6917bedbf8babf389c56ba25c
Dhani5703 <dems9811@naver.com>
2024-12-05 00:14:24 +0900

    [fix] prettier 수정 - 파일 내 따옴표 통일

--- src/pages/items/[itemId].tsx
+++ src/pages/items/[itemId].tsx
@@ -1,7 +1,7 @@
 // pages/todo/[itemId].tsx
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Image from 'next/image';
+import Image from "next/image";
 import {
   fetchTodoItemById,
   updateTodoItem,
