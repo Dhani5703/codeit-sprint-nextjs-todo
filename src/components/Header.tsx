308ecdfa4802965f2faa7dc43806f59feb9f65c1
Dhani5703 <dems9811@naver.com>
2024-12-12 01:28:35 +0900

    [feat] header 반응형 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
+  const [logo, setLogo] = useState("/Size=Large.png");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setLogo("/Size=Small.png");
+      } else {
+        setLogo("/Size=Large.png");
+      }
+    };
+
+    // 초기화 및 리스너 설정
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <header className="w-full h-10 flex items-center bg-white m-0 p-6">
+    <header className="w-full h-16 flex items-center bg-white m-0 px-6">
       <button
         onClick={() => (window.location.href = "/")}
-        className="ml-[20%] h-[50px] w-[150px] bg-center bg-no-repeat bg-transparent"
+        className="h-[50px] w-[150px] bg-center bg-no-repeat bg-transparent md:ml-[20%] ml-0"
         style={{
-          backgroundImage: "url('/Size=Large.png')",
+          backgroundImage: `url('${logo}')`,
         }}
       ></button>
     </header>
