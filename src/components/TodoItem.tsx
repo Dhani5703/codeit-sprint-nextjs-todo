42f1a363a87edbd6d531b115d87866f72495d34c
Dhani5703 <dems9811@naver.com>
2024-12-12 01:46:55 +0900

    [style] todo 텍스트 중앙정렬

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -24,43 +24,47 @@ const TodoItem: React.FC<TodoItemProps> = ({
 }) => {
   return (
     <div
-      className="todo-item flex items-center p-2 mb-2 border-b bg-center bg-no-repeat bg-cover justify-between"
+  className="todo-item flex items-center p-2 mb-2 border-b bg-center bg-no-repeat bg-cover"
+  style={{
+    backgroundImage: `url('/Property 1=${completed ? "Variant2" : "Default"}.png')`,
+    backgroundSize: "100% 100%",
+  }}
+>
+  {/* onToggleComplete 이미지 */}
+  <Image
+    src={`/Property 1=${completed ? "Done" : "Todo"}.png`}
+    alt={completed ? "Done" : "Todo"}
+    onClick={() => onToggleComplete(id, completed)}
+    className="cursor-pointer w-6 h-6 mr-2"
+    width={32}
+    height={32}
+  />
+  {/* 텍스트 영역 */}
+  {isEditable ? (
+    <div className="flex items-center flex-1 bg-transparent">
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => onEditName?.(e.target.value)} // 이름 변경 처리
+        className="flex-1 p-2 mr-3 rounded border-none outline-none text-center"
+      />
+    </div>
+  ) : (
+    <span
+      onClick={isDetailPage ? undefined : () => onViewDetails?.(id)}
+      className={`flex-1 ${completed ? "text-gray-400" : ""} cursor-pointer truncate`}
       style={{
-        backgroundImage: `url('/Property 1=${completed ? "Variant2" : "Default"}.png')`,
-        backgroundSize: "100% 100%",
+        pointerEvents: isDetailPage ? "none" : "auto",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textAlign: "center",  // 텍스트 중앙 정렬 추가
       }}
     >
-      <Image
-        src={`/Property 1=${completed ? "Done" : "Todo"}.png`}
-        alt={completed ? "Done" : "Todo"}
-        onClick={() => onToggleComplete(id, completed)}
-        className="cursor-pointer w-6 h-6"
-        width={32}
-        height={32}
-      />
-      {isEditable ? (
-        <div className="flex items-center flex-1 bg-transparent border-none outline-none ">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => onEditName?.(e.target.value)} // 이름 변경 처리
-            className="flex-1 p-2 rounded border-none outline-none"
-          />
-        </div>
-      ) : (
-        <span
-          onClick={isDetailPage ? undefined : () => onViewDetails?.(id)}
-          className={`flex-1 ${completed ? "text-gray-400" : ""} cursor-pointer truncate`}
-          style={{
-            pointerEvents: isDetailPage ? "none" : "auto",
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-          }}
-        >
-          {name}
-        </span>
-      )}
-    </div>
+      {name}
+    </span>
+  )}
+</div>
+
   );
 };
 
