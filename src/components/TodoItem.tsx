3b31df945b2b70953a6e279c324723138ce67adc
Dhani5703 <dems9811@naver.com>
2024-12-11 22:39:01 +0900

    [fix[ prettier 적용

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -27,28 +27,28 @@ const TodoItem: React.FC<TodoItemProps> = ({
       className="todo-item flex items-center p-2 mb-2 border-b bg-center bg-no-repeat bg-cover justify-between"
       style={{
         backgroundImage: `url('/Property 1=${completed ? "Variant2" : "Default"}.png')`,
-        backgroundSize:'100% 100%'
+        backgroundSize: "100% 100%",
       }}
     >
-    <Image
-      src={`/Property 1=${completed ? "Done" : "Todo"}.png`}
-      alt={completed ? "Done" : "Todo"}
-      onClick={() => onToggleComplete(id, completed)}
-      className="cursor-pointer w-6 h-6"
-      width={32}
-      height={32}
-    />
-    {isEditable ? (
-      <div className="flex items-center flex-1 bg-transparent">
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => onEditName?.(e.target.value)} // 이름 변경 처리
-          className="flex-1 p-2 rounded"
-        />
+      <Image
+        src={`/Property 1=${completed ? "Done" : "Todo"}.png`}
+        alt={completed ? "Done" : "Todo"}
+        onClick={() => onToggleComplete(id, completed)}
+        className="cursor-pointer w-6 h-6"
+        width={32}
+        height={32}
+      />
+      {isEditable ? (
+        <div className="flex items-center flex-1 bg-transparent">
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => onEditName?.(e.target.value)} // 이름 변경 처리
+            className="flex-1 p-2 rounded"
+          />
         </div>
       ) : (
-        <span 
+        <span
           onClick={isDetailPage ? undefined : () => onViewDetails?.(id)}
           className={`flex-1 ${completed ? "text-gray-400" : ""} cursor-pointer truncate`}
           style={{
